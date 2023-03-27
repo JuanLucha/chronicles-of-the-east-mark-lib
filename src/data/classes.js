3d4f989885eb6a43a2986cff3e235c40c91d1030
Juan Adolfo Lucha García <juanlucha@gmail.com>
2023-03-28 00:53:34 +0200

    Barbarian class added

--- src/data/classes.js
+++ src/data/classes.js
@@ -86,7 +86,92 @@ const classes = {
       }
     }
   },
-  Barbarian: "Bárbaro",
+  Barbarian: {
+    name: "Bárbaro",
+    mainAttribute: "str",
+    hitDice: 12,
+    weapons: ["all"],
+    armor: ["all"],
+    level: {
+      1: {
+        experiencePoints: 0,
+        attackBonus: 0,
+        hitDice: ["1d6"],
+        abilities: [
+          "Alerta",
+          "Instintos primitivos",
+          "Intimidar (CON)",
+          "Rastreador nato"
+        ]
+      },
+      2: {
+        experiencePoints: 1751,
+        attackBonus: 1,
+        hitDice: ["1d12"],
+        abilities: []
+      },
+      3: {
+        experiencePoints: 3501,
+        attackBonus: 2,
+        hitDice: ["1d12"],
+        abilities: []
+      },
+      4: {
+        experiencePoints: 7001,
+        attackBonus: 3,
+        hitDice: ["1d12"],
+        abilities: ["Furia desatada"]
+      },
+      5: {
+        experiencePoints: 14001,
+        attackBonus: 4,
+        hitDice: ["1d12"],
+        abilities: []
+      },
+      6: {
+        experiencePoints: 25001,
+        attackBonus: 5,
+        hitDice: ["1d12"],
+        abilities: ["Aguante"]
+      },
+      7: {
+        experiencePoints: 50001,
+        attackBonus: 6,
+        hitDice: ["1d12"],
+        abilities: []
+      },
+      8: {
+        experiencePoints: 90001,
+        attackBonus: 7,
+        hitDice: ["1d12"],
+        abilities: []
+      },
+      9: {
+        experiencePoints: 150001,
+        attackBonus: 8,
+        hitDice: ["1d12"],
+        abilities: []
+      },
+      10: {
+        experiencePoints: 200001,
+        attackBonus: 9,
+        hitDice: ["1d12"],
+        abilities: ["Llamada ancestral"]
+      },
+      11: {
+        experiencePoints: 350001,
+        attackBonus: 10,
+        hitDice: ["+5 PG"],
+        abilities: []
+      },
+      12: {
+        experiencePoints: 500001,
+        attackBonus: 11,
+        hitDice: ["+5 PG"],
+        abilities: []
+      }
+    }
+  },
   Bard: "Bardo",
   Knight: "Caballero",
   Cleric: "Clérigo",
