339f144d77918bfb482d6c2831c392cb22c8e619
Juan Lucha <juanlucha@gmail.com>
2025-06-04 01:54:50 +0200

    refactor: clean up formatting in vite.config.js for consistency

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/index.js', // Cambia esto si tu entry point es diferente
-      name: 'CronicasDeLaMarcaLib',
-      fileName: (format) => `cronicas-de-la-marca-lib.${format}.js`,
-      formats: ['es', 'cjs']
-    },
-    rollupOptions: {
-      // Excluye dependencias externas del bundle
-      external: [],
-      output: {}
+      entry: "src/index.js",
+      name: "CronicasDeLaMarcaLib",
+      fileName: format => `cronicas-de-la-marca-lib.${format}.js`,
+      formats: ["es", "cjs"]
     }
   },
   test: {
-    // Opciones de Vitest (puedes personalizar según tus necesidades)
     globals: true,
-    environment: 'node'
+    environment: "node"
   }
-})
+});
