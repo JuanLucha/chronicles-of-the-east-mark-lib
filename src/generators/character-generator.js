22f13cc144843245b477120416025d82070de22f
Juan Lucha <juanlucha@gmail.com>
2025-06-04 01:48:59 +0200

    refactor: standardize class key usage in character generation

--- src/generators/character-generator.js
+++ src/generators/character-generator.js
@@ -35,34 +35,34 @@ const generateCharacter = (name, ancestry, characterClass, level) => {
   character.ancestryTraits = ancestries[ancestry].traits;
 
   // Fill the traits with the ones in the classes array
-  const { level: _, ...rest } = classes[characterClass];
+  const classKey = characterClass.toLowerCase();
+  const { level: _, ...rest } = classes[classKey];
   character.classTraits = rest;
 
   // Add the hit points based on the class and level
   character.hitPoints = hpGenerator(
     level,
-    classes[characterClass].hitDice,
-    classes[characterClass].extraHitPoints,
+    classes[classKey].hitDice,
+    classes[classKey].extraHitPoints,
     getModifier(character.stats.con)
   );
 
   // Add the attack bonus based on the class and level
-  character.attackBonus = classes[characterClass].level[level].attackBonus;
+  character.attackBonus = classes[classKey].level[level].attackBonus;
 
   // Add the experience points based on the class and level
-  character.experiencePoints =
-    classes[characterClass].level[level].experiencePoints;
+  character.experiencePoints = classes[classKey].level[level].experiencePoints;
 
   // Add the abilities based on the class and level, adding all the abilities from previous levels together
   for (let i = 1; i <= level; i++) {
     character.classTraits.abilities = [
       ...(character.classTraits.abilities || []),
-      ...(classes[characterClass].level[i].abilities || [])
+      ...(classes[classKey].level[i].abilities || [])
     ];
   }
 
   // Select the primary stats based on the class and ancestry
-  character.primaryStats.push(classes[characterClass].mainAttribute);
+  character.primaryStats.push(classes[classKey].mainAttribute);
   character.primaryStats = generatePrimaryStats(character.primaryStats);
   if (character.ancestry === "human") {
     character.primaryStats = generatePrimaryStats(character.primaryStats);
